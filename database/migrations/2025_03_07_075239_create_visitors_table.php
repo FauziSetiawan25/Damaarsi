82538873822714589ccda8ad28a5fbe97f3eacb1
FauziSetiawan25 <fauzisetiawan2003@mail.ugm.ac.id>
2025-03-21 14:22:18 +0700

    mengubah migration visitor

--- database/migrations/2025_03_07_075239_create_visitors_table.php
+++ database/migrations/2025_03_07_075239_create_visitors_table.php
@@ -4,25 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up()
     {
         Schema::create('visitors', function (Blueprint $table) {
             $table->id();
             $table->string('ip_address');
-            $table->string('user_agent')->nullable();
+            $table->text('user_agent')->nullable();
             $table->timestamp('visited_at')->useCurrent();
+            $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('visitors');
     }
